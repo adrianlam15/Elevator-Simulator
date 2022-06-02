b78b037b6455e408c10386728ed28cf2cc8ab955
Adrian <adrianlam1511@gmail.com>
2022-06-01 21:05:18 -0400

    added assets

--- states/launch_settings.py
+++ states/launch_settings.py
@@ -6,17 +6,17 @@ from states.title import title
 class setting_state(state_format):
     def __init__(self, game):
         super().__init__(game)
-        self.surface = pygame.Surface()
+        self.surface = pygame.Surface((672, 378))
         self.select = {"Resolution": None, "Sound": False}
-        self.actions = {"Start": False}
 
-    def update(self):
-        if self.actions["Start"]:
+    def update(self, actions):
+        if actions["Click"]:
             next_state = title(self.game)
             next_state.enter_state()
+        self.game.reset_keys()
 
     def render(self, surface):
-        pass
+        surface.blit(self.surface, (0, 0))
 
 
 class drop_down:
