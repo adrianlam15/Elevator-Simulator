2b1ef3a94d0d7721f62e349ad282549102acd113
Adrian <adrianlam1511@gmail.com>
2022-06-02 23:23:59 -0400

    modified asset files

--- states/game_state.py
+++ states/game_state.py
@@ -8,37 +8,32 @@ class main_game(state_format):
         super().__init__(game)
         self.surface = pygame.Surface((672, 378))
         self.surface.fill("White")
-        self.elevator = elevator(self.game, True, self.surface)
+        self.elevator1 = elevator(self.game, True, self.surface)
+        self.elevator2 = elevator(self.game, False, self.surface)
 
     def update(self, actions):
         if actions["Pause"]:
             next_state = paused(self.game)
             next_state.enter_state()
-        self.elevator.update()
+
+        self.elevator1.update(actions)
         self.game.reset_keys()
 
         # elevator velocity changes go here
 
     def render(self, surface):
-        self.elevator.render(self.surface)
+        self.elevator1.render(self.surface)
         surface.blit(self.surface, (0, 0))
 
 
 class button(pygame.sprite.Sprite):
-    def __init__(self, elevator, game, file):
+    def __init__(self, elevator, game, file, x, y):
         super().__init__()
         self.elevator = elevator
         self.game = game
         self.file = file
+        self.x, self.y = x, y
         self.dir = os.path.join("assets", "graphics", "Keys")
-
-    def get_button(self, x, y, w, h):
-        button = pygame.Surface((w, h))
-        button.set_colorkey((0, 0, 0))
-        button.blit(self.image, (0, 0), (x, y, w, h))
-        self.rect = button.get_rect(topleft=(0, 0))
-        print(self.rect)
-        return button
 
     def update(self, action):
         if action["Clicked"]:
@@ -66,39 +61,14 @@ class elevator:
             "5": False,
             "6": False,
         }
-        self.button_surface = pygame.Surface((672, 378))
         self.button_config = "button_coords.json"
         self.button_group = []
-        self.button_click_group = []
         with open(self.button_config, "r") as input:
             self.data = json.load(input)
         input.close()
-        for elem in self.data["frames"]:
-            buttons = button(self, self.game, self.button_config)
-            buttons.button_val = elem[0]
-            buttons.x, buttons.y, buttons.w, buttons.h = (
-                self.data["frames"][elem]["coords"]["x"],
-                self.data["frames"][elem]["coords"]["y"],
-                self.data["frames"][elem]["coords"]["w"],
-                self.data["frames"][elem]["coords"]["h"],
-            )
 
-            buttons.image = pygame.image.load(os.path.join(buttons.dir, elem))
-            if self.service:
-                """buttons.surface = pygame.Surface((buttons.w, buttons.h))"""
-                buttons.get_button(buttons.x, buttons.y, buttons.w, buttons.h)
-            else:
-                if elem == "S-Key.png":
-                    break
-                buttons.get_button(buttons.x, buttons.y, buttons.w, buttons.h)
-            if elem[2] == "5":
-                self.button_click_group.append(buttons)
-                print("Added click")
-            else:
-                self.button_group.append(buttons)
-                print("Added")
-
-    def update(self):
+    def update(self, actions):
+        self.button_update()
         if self.direction["Up"] is True:
             self.direction["Down"] = False
         elif self.direction["Down"] is True:
@@ -111,5 +81,28 @@ class elevator:
             for keys in self.direction.keys():
                 self.direction[keys] = False
 
+    def button_update(self):
+        for elem in self.data["frames"]:
+            x, y = 200, 200
+            buttons = button(self, self.game, self.button_config, x, y)
+            print(x)
+            buttons.button_val = elem[0]
+            buttons.w, buttons.h = (
+                self.data["frames"][elem]["size"]["w"],
+                self.data["frames"][elem]["size"]["h"],
+            )
+            buttons.image = pygame.image.load(os.path.join(buttons.dir, elem))
+            if self.service:
+                buttons.image = pygame.image.load(os.path.join(buttons.dir, elem))
+            else:
+                if elem == "S-Key.png":
+                    break
+                buttons.image = pygame.image.load(os.path.join(buttons.dir, elem))
+            buttons.rect = buttons.image.get_rect(topleft=(buttons.x, buttons.y))
+            self.button_group.append(buttons)
+            x += 50
+        self.button_group = pygame.sprite.Group(buttons)
+
     def render(self, surface):
-        surface.blit()
+        """self.buttons.draw(surface)"""
+        pass
