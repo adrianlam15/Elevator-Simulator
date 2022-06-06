778537b9aba159dddc5cbd8ab7c94f617fe8782d
Adrian <adrianlam1511@gmail.com>
2022-06-06 17:06:28 -0400

    added elevators

--- states/game_state.py
+++ states/game_state.py
@@ -2,10 +2,16 @@ import pygame, os, json, time
 from states.state import state_format
 from states.pause_menu import paused
 
+with open("button_coords.json", "r") as input:
+    data = json.load(input)
+input.close()
+
 
 class main_game(state_format):
     def __init__(self, game):
         super().__init__(game)
+        service = True
+
         self.surface = pygame.Surface((672, 378))
         self.surface.fill((40, 42, 54))
         self.music = pygame.mixer.Sound(
@@ -13,8 +19,21 @@ class main_game(state_format):
         )
         if self.game.sound_enabled:
             self.music.play(1, 0, 500)
+        self.elevator_group = []
+
         self.elevator1 = elevator(self.game, True, self.surface)
         self.elevator2 = elevator(self.game, False, self.surface)
+        self.elevator_group.append(self.elevator1)
+        self.elevator_group.append(self.elevator2)
+        print(data["frames"]["Elevator Pictures"])
+        for elevator_obj in self.elevator_group:
+            for image_name in data["frames"]["Elevator Pictures"]:
+                img_load = pygame.image.load(
+                    os.path.join(
+                        self.game.asset_dir, "graphics", "elevator", image_name
+                    )
+                )
+                elevator_obj.images.append(img_load)
         self.elevator1.button_init(300, 340)
         self.elevator2.button_init(364, 340)
 
@@ -61,7 +80,7 @@ class button(pygame.sprite.Sprite):
 class elevator:
     def __init__(self, game, service, surface):
         self.game = game
-        self.image = ""
+        self.images = []
         self.service = service
         self.surface = surface  # temp
         self.rect = self.surface.get_rect()
@@ -75,12 +94,8 @@ class elevator:
             "5": False,
             "6": False,
         }
-        self.button_config = "button_coords.json"
         self.button_group = []
         self.floor_queue = []
-        with open(self.button_config, "r") as input:
-            self.data = json.load(input)
-        input.close()
 
     def button_collision_detection(self, actions):
         for self.button in self.button_group:
@@ -106,13 +121,13 @@ class elevator:
         if self.door_state["Open"] is True:
             self.door_state["Closed"] = False
         elif self.door_state["Open"] is False:
-            self.door_state["Closeded"] = True
+            self.door_state["Closed"] = True
         if self.door_state["Open"] is True:
             for keys in self.direction.keys():
                 self.direction[keys] = False
 
     def button_init(self, x=300, y=340):
-        for elem in self.data["frames"]["elevator buttons"]:
+        for elem in data["frames"]["elevator buttons"]:
             if not self.service:
                 print(self.service)
                 if elem == "S-Key.png":
@@ -120,8 +135,8 @@ class elevator:
             val = elem[0]
             self.buttons = button(self, self.game, x, y, elem, val)
             self.buttons.w, self.buttons.h = (
-                self.data["frames"]["elevator buttons"][elem]["size"]["w"],
-                self.data["frames"]["elevator buttons"][elem]["size"]["h"],
+                data["frames"]["elevator buttons"][elem]["size"]["w"],
+                data["frames"]["elevator buttons"][elem]["size"]["h"],
             )
             y -= 30
             self.button_group.append(self.buttons)
